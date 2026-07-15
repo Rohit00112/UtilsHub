12d9895a714355c348712105d023b480380e11bd
rohit00112 <subedirohit49@gmail.com>
2026-07-15 09:04:26 +0545

    add microsoft clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,13 @@ gtag('config', '${gaId}');`}
                         </Script>
                     </>
                 )}
+                <Script id="ms-clarity" strategy="afterInteractive">
+                    {`(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "xmmy8i6fh8");`}
+                </Script>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
